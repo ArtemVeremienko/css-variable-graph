e835d6504cd2ba5dfd8bedf4f0d95563d4540992
Artem Veremienko <temaspsxxx@gmail.com>
2026-07-14 23:57:39 +0300

    Make Vite base path dynamic using GITHUB_REPOSITORY

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/css-variable-graph/' : '/',
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   server: {
     port: 3000,
     open: true
